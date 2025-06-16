101093f2d00da2b23e62b521498fcb1114654929
AlexandrTsygun <a.tsygun@mail.ru>
2025-06-16 23:26:33 +0300

    first lesson

---
+++ folder-app/folder-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button/Button.css';
+import { Card } from './components/Button/Button';
+
+function App() {
+  return (
+
+    // <div className="App">
+    //   <Button buttonText="button"/>
+    //   <Button buttonText="button"/>
+    //   <Button buttonText="button"/>
+    //   <Button buttonText="button"/>
+    //   <Button buttonText="button"/>
+    // </div>
+
+    <div>
+      <Card h2="Consistency" p="Podcasting operational change management  workflow."/>
+    </div>
+  );
+}
+
+export default App;
